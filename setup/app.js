2f92581448d3982f343ad049de45bc55b84af6f7
Lishan <lishan.low11@gmail.com>
2023-05-09 17:44:42 +0100

    declared variables

---
+++ setup/app.js
@@ -0,0 +1,14 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+        
+    }
+
+})
